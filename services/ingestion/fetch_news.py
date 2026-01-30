f3e64724a188176d72319a630e75952ed1da2d61
Phillip Ngo <philip.ogn@gmail.com>
2026-01-30 21:26:31 +0000

    fetching pages testing

--- services/ingestion/fetch_news.py
+++ services/ingestion/fetch_news.py
@@ -14,8 +14,27 @@ if NEWS_API_KEY is None:
 
 newsapi = NewsApiClient(api_key=NEWS_API_KEY)
 
-top_headlines = newsapi.get_top_headlines(sources='bbc-news',
-                                          language='en')
+# top_headlines = newsapi.get_top_headlines(sources='bbc-news',
+#                                           language='en')
+
+def get_news_articles():
+    all_articles = []
+    for i in range(5):
+        all_headlines = newsapi.get_everything(q='politics', page=i+1)
+        all_articles.extend(all_headlines['articles'])
+
+    with open('every_headline.json', 'w', encoding='utf-8') as f:
+        json.dump(all_articles, f, indent=2)
+
+get_news_articles()
+
+def load_and_count():
+    with open('every_headline.json', 'r') as f:
+        headlines = json.load(f)
+    print(len(headlines['articles']))
+
+# load_and_count()
+
 
 # with open('top_headlines.json', 'w', encoding='utf-8') as f:
 #     json.dump(top_headlines, f, indent=2, ensure_ascii=False)
@@ -42,8 +61,8 @@ def save_article(article_data):
     finally:
         db.close()
 
-for a in top_headlines['articles']:
-    save_article(a)
+# for a in top_headlines['articles']:
+#     save_article(a)
 
 '''    article_id = Column(String, primary_key=True)
     source = Column(String, nullable=False)
